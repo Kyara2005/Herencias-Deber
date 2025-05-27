7d3481f1c3dbc98f1595c380c9d55146f1f10677
Kyara2005 <kyara.altamirano@epn.edu.ec>
2025-05-26 21:31:56 -0500

    1,2 actividad

---
+++ src/Cliente.java
@@ -0,0 +1,10 @@
+public class Cliente extends Persona {
+    private String numeroCuenta;
+    private double saldo;
+
+    public Cliente(String nombre, String cedula, String direccion, String telefono, String numeroCuenta, double saldo) {
+        super(nombre, cedula, direccion, telefono);
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+}
